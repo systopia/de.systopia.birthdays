f244f80e3a24b1972e4b3538de2bbc955a4ad231
Jo Franz <franz@systopia.de>
2022-11-22 12:02:34 +0100

    Updated links

--- CRM/Birthdays/Form/Report/Birthdays.mgd.php
+++ CRM/Birthdays/Form/Report/Birthdays.mgd.php
@@ -2,7 +2,7 @@
 // This file declares a managed database record of type "ReportTemplate".
 // The record will be automatically inserted, updated, or deleted from the
 // database as appropriate. For more details, see "hook_civicrm_managed" at:
-// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+// https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
 return array (
   0 => 
   array (
--- birthdays.php
+++ birthdays.php
@@ -18,7 +18,7 @@ require_once 'birthdays.civix.php';
 /**
  * Implements hook_civicrm_config().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config
  */
 function birthdays_civicrm_config(&$config) {
   _birthdays_civix_civicrm_config($config);
@@ -27,7 +27,7 @@ function birthdays_civicrm_config(&$config) {
 /**
  * Implements hook_civicrm_install().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
  */
 function birthdays_civicrm_install() {
   _birthdays_civix_civicrm_install();
@@ -36,7 +36,7 @@ function birthdays_civicrm_install() {
 /**
  * Implements hook_civicrm_uninstall().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall/
  */
 function birthdays_civicrm_uninstall() {
   _birthdays_civix_civicrm_uninstall();
