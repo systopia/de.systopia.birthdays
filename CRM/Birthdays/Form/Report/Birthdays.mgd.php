9dc3ceff32dddc14da2f0d0ed72b672f634fd278
Jo Franz <franz@systopia.de>
2022-11-22 12:20:37 +0100

    automatic replace array() notation to [ ] short syntax

--- CRM/Birthdays/Form/Report/Birthdays.mgd.php
+++ CRM/Birthdays/Form/Report/Birthdays.mgd.php
@@ -3,19 +3,19 @@
 // The record will be automatically inserted, updated, or deleted from the
 // database as appropriate. For more details, see "hook_civicrm_managed" at:
 // https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
-return array (
+return [
   0 => 
-  array (
-    'name' => ts('Birthday Report', array('domain' => 'de.systopia.birthdays')),
+  [
+    'name' => ts('Birthday Report', ['domain' => 'de.systopia.birthdays']),
     'entity' => 'ReportTemplate',
     'params' => 
-    array (
+    [
       'version' => 3,
-      'label' => ts('Birthdays', array('domain' => 'de.systopia.birthdays')),
-      'description' => ts('Report on upcoming birthdays', array('domain' => 'de.systopia.birthdays')),
+      'label' => ts('Birthdays', ['domain' => 'de.systopia.birthdays']),
+      'description' => ts('Report on upcoming birthdays', ['domain' => 'de.systopia.birthdays']),
       'class_name' => 'CRM_Birthdays_Form_Report_Birthdays',
       'report_url' => 'contact/birthdays',
       'component' => '',
-    ),
-  ),
-);
+    ],
+  ],
+];
