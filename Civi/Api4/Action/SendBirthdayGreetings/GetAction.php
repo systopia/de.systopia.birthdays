d1fdf905e2f60a26e6ee05e2be62035e9160b90a
Jo Franz <franz@systopia.de>
2022-11-24 12:51:57 +0100

    Initial apiv4 commit

---
+++ Civi/Api4/Action/SendBirthdayGreetings/GetAction.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Copyright (C) 2022 SYSTOPIA GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation in version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+namespace Civi\Api4\Action\SendBirthdayGreetings;
+
+use Civi\Api4\SendBirthdayGreetings;
+use Civi\Api4\Generic\AbstractGetAction;
+use Civi\Api4\Generic\Result;
+
+final class GetAction extends AbstractGetAction {
+
+  public function __construct() {
+  }
+
+  /**
+   * @inheritDoc
+   *
+   * @throws \API_Exception
+   */
+  public function _run(Result $result): void {
+      $result[] = [
+          'row' => 0,
+          'key' => 'test'
+      ];
+
+  }
+}
---
+++ Civi/Api4/SendBirthdayGreetings.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Copyright (C) 2022 SYSTOPIA GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation in version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+namespace Civi\Api4;
+
+use Civi\Api4\Generic\AbstractEntity;
+use Civi\Api4\Action\SendBirthdayGreetings\GetAction;
+
+final class SendBirthdayGreetings extends AbstractEntity {
+
+    public static function get(): GetAction {
+        return \Civi::service(GetAction::class);
+    }
+
+}
