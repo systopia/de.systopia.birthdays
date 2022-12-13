5f69a9abb6776a259071c6b7ba47be4e10abe987
Jo Franz <franz@systopia.de>
2022-12-13 14:27:17 +0100

    Mailing class

---
+++ CRM/Birthdays/Mailer.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Copyright (C) 2022 SYSTOPIA GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation in version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class CRM_Birthdays_Mailer
+{
+
+    public function __construct()
+    {
+    }
+
+    public function send_mails() {
+
+        $template_name = 'Sample CiviMail Newsletter Template'; // fixme test
+
+
+        echo "test";
+    }
+}
