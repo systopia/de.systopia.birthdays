f0eec635f2c7c140abaa17ca5d2d8a3dff23ee7b
Jo Franz <franz@systopia.de>
2022-12-30 22:58:41 +0100

    Calculate and return contact age

--- CRM/Birthdays/BirthdayTokens.php
+++ CRM/Birthdays/BirthdayTokens.php
@@ -17,16 +17,34 @@ class CRM_Birthdays_BirthdayTokens
      * Handles civicrm_tokenValues hook
      * @param $values - array of values, keyed by contact id
      * @param $contact_ids - array of contactIDs that the system needs values for.
-     * @param $job - the job_id
-     * @param $tokens - tokens used in the mailing - use this to check whether a token is being used and avoid fetching data for unneeded tokens
-     * @param $context - the class name
+     * @param null $job - the job_id
+     * @param array $tokens - tokens used in the mailing - use this to check whether a token is being used and avoid fetching data for unneeded tokens
+     * @param null $context - the class name
      *
      * @see https://docs.civicrm.org/dev/en/master/hooks/hook_civicrm_tokenValues
      */
     public static function tokenValues(&$values, $contact_ids, $job = null, $tokens = [], $context = null): void
     {
-        foreach ($contact_ids as $contact_id) {
-            $values[$contact_id]['contact.age'] = 'EXAMPLE-fixme!!';
+        try {
+            $contacts = \Civi\Api4\Contact::get()
+                ->addSelect('id', 'birth_date')
+                ->addWhere('id', 'IN', $contact_ids)
+                ->execute();
+            foreach ($contacts  as $key => $contact_info ) {
+                $age = self::calculate_birthday($contact_info['birth_date']);
+                $values[$contact_info['id']]['contact.age'] = $age;
+            }
+        } catch (Exception $exception) {
+            Civi::log()->debug("Birthdays: Failed to fetch and  calculate age: $exception");
         }
+    }
+
+    /**
+     * @param string $iso_birth_date iso birthdate
+     * @return int
+     */
+    private static function calculate_birthday(string $iso_birth_date): int
+    {
+        return date_diff(date_create($iso_birth_date), date_create('now'))->y;
     }
 }
