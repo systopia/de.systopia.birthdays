2070141323d1fc7d9de5184234302118d9f19dd7
Jens Schuppe <schuppe@systopia.de>
2026-03-06 13:04:29 +0100

    Set version to 1.7.0-beta1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.birthdays',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e472ab6bc6bb6082eddd6316a0301b68c5549b73',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.birthdays' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e472ab6bc6bb6082eddd6316a0301b68c5549b73',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
